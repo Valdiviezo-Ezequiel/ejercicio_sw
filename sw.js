94404ce48f5518b3a389413b3be64a631f1fa340
Valdiviezo-Ezequiel <ezequiel.valdiviezo@davinci.edu.ar>
2023-04-24 22:09:39 -0300

    feat: add users request

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
-console.log("SW i am service worker");
+console.log("SW: i am service worker");
+
+self.addEventListener('fetch', event => {
+    console.log("SW: ", event.request.url);
+    console.log(event);
+})
+
+fetch('https://reqres.in/api/users')
+    .then(Response => Response.json())
+    .then(json => console.log(json));
